738104a98d65ff23bbc70c1ccdfdf948b64beeb9
Britney Frei <britneyfrei19@gmail.com>
2023-06-18 17:24:42 -0400

    edits for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const helmet = require("helmet");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 
@@ -12,18 +13,15 @@ const transactions = require("./routes/transactions");
 
 const app = express();
 
-app.use(
-	cors({
-		origin: "https://expense-tracker-frontend-ae8z.onrender.com/",
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-	})
-);
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(cors());
 
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+
 app.use("/api/v1/transactions", transactions);
 
 app.get("/", (req, res) => res.send("hello"));
